ce05057428e526d3efb492780fd56b06782685e7
shufangxun <1623062935@qq.com>
2019-04-09 21:39:18 +0800

    2019-04-09

--- Array/ReorderArray.py
+++ Array/ReorderArray.py
@@ -1,7 +1,6 @@
 import collections
 def reorderArray(array):
-    ##将数组的奇数置前，偶数置后，保持元素间的相对位置
-        
+    ##将数组的奇数置前，偶数置后，保持元素间的相对位置   
     '''
     solution 1
     sorted(iterable,key,reverse=False) 默认是升序
@@ -41,4 +40,6 @@ def reorderArray(array):
     return array      
 
 
-    
+if __name__ == "__main__":
+     arr = [1,2,3,4,5]
+     print(reorderArray(arr))   
