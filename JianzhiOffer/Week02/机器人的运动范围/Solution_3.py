292586dff6a890e5ae32768f59a188a7f53f63b7
shufangxun <1623062935@qq.com>
2019-06-24 11:16:17 +0800

    fixed

--- JianzhiOffer/Week02/机器人的运动范围/Solution_3.py
+++ JianzhiOffer/Week02/机器人的运动范围/Solution_3.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution(object):
     def movingCount(self, threshold, rows, cols):
         """
@@ -6,3 +7,29 @@ class Solution(object):
         :type cols: int
         :rtype: int
         """
+        if not rows or not cols:
+            return 0
+        res = 1
+        memo = set()
+        memo.add((0,0))
+        q = deque([(0,0)])
+        dx = [-1, 0, 1, 0]
+        dy = [0, 1, 0, -1]
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                xx = x + dx[i]
+                yy = y + dy[i]
+                if self.is_valid(xx, yy, rows, cols, threshold) and (xx, yy) not in memo:
+                    res += 1
+                    q.append((xx, yy))
+                    memo.add((xx, yy))
+        return res
+
+    def is_valid(self, x, y, m, n, k):
+        return 0 <= x < m and 0 <= y < n and self.sumxy(x, y) <= k
+
+    def sumxy(self, x, y):      
+        return x % 10 + x // 10 + y % 10 + y // 10
+
+ 
