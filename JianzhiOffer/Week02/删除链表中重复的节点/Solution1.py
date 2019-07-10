4f3b1b74a5da9ee114750784425f316b11e539da
shufangxun <1623062935@qq.com>
2019-07-10 14:47:12 +0800

    Update Solution1.py

--- JianzhiOffer/Week02/删除链表中重复的节点/Solution1.py
+++ JianzhiOffer/Week02/删除链表中重复的节点/Solution1.py
@@ -10,7 +10,7 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # 注意排序这个关键点
+        # 保留一个重复结点
         if head is None or head.next is None:
             return head
         
