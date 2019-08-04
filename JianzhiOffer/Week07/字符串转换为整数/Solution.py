21af96412bdf32781fb601d77e720ab6acbe3d84
shufangxun <1623062935@qq.com>
2019-08-04 21:47:24 +0800

    update

--- JianzhiOffer/Week07/字符串转换为整数/Solution.py
+++ JianzhiOffer/Week07/字符串转换为整数/Solution.py
@@ -4,3 +4,28 @@ class Solution(object):
         :type str: str
         :rtype: int
         """
+        if not str:
+            return 0
+        flag = 1
+        str = str.lstrip() # 左边空格全部消除
+        if str[0] == '+':
+            str = str[1:]
+        elif str[0] == '-':
+            flag = -1
+            str = str[1:]
+        res = 0
+        i = 0
+        while i < len(str):
+            if str[i].isdigit():
+                res *= 10
+                res += int(str[i]) 
+                i += 1
+            else:
+                break
+                
+        res *= flag
+        if res > 2 ** 31 - 1:
+             return  2 ** 31 - 1 
+        if res < - 2 ** 31:
+             return  - 2 ** 31 
+        return res 
