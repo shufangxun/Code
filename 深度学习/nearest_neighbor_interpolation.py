ca09035d27ad9e944a322d3b5445ae815efd0309
shufangxun <1623062935@qq.com>
2019-08-24 21:48:35 +0800

    update

---
+++ 深度学习/nearest_neighbor_interpolation.py
@@ -0,0 +1,13 @@
+import numpy as np
+def resize(src, dst_h, dst_w):
+    src_h, src_w, channels = src.shape
+    ratio_h = src_h / dst_h
+    ratio_w = src_w / dst_h
+    dst = np.zeros((dst_h, dst_w), np.int8)
+    for i in range(channels):
+        for y in range(dst_h):
+            for x in range(dst_w):
+                x_ori = int(x * ratio_w)
+                y_ori = int(y * ratio_h)
+                dst[y, x, i] = src[y_ori, x_ori, i]
+    return dst
