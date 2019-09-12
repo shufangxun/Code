d1e2fbd517a8c3e532e707b0829e509b3edf5d8b
shufangxun <1623062935@qq.com>
2019-09-12 19:37:43 +0800

    Create matrix_mul.py

---
+++ 深度学习/matrix_mul.py
@@ -0,0 +1,16 @@
+def matrix_multiply(A, B):
+    m, p = len(A), len(A[0])
+    n = len(B[0])
+    if p != len(B):
+        raise ValueError("A和B维度不匹配")
+    C = [[0 for i in range(n)] for j in range(m)]
+    for i in range(m):
+        for j in range(n):
+            for k in range(p):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+if __name__ == "__main__":
+    A = [[1,2,3],[1,2,3],[4,5,6]]
+    B = [[1,2,3,0],[1,2,3,1],[4,5,6,9]]
+    print(matrix_multiply(A,B))
