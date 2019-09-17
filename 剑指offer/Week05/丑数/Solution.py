37cd5238389fa6fea74885671031dc0bd5d7bca4
shufangxun <1623062935@qq.com>
2019-09-17 13:47:50 +0800

    update

--- 剑指offer/Week05/丑数/Solution.py
+++ 剑指offer/Week05/丑数/Solution.py
@@ -8,11 +8,11 @@ class Solution(object):
         res = [0] * n # 开辟空间,不能res = []
         res[0] = 1
         for s in range(1,n):
-            res[s] = min(res[i]*2, min(res[j]*3, res[k]*5))
-            if res[s] == res[i]*2:
+            res[s] = min(res[i] * 2, min(res[j] * 3, res[k] * 5))
+            if res[s] == res[i] * 2:
                 i += 1
-            if res[s] == res[j]*3: # 不用else是为了去重: 6 = 2 * 3
+            if res[s] == res[j] * 3: # 不用else是为了去重: 6 = 2 * 3
                 j += 1
-            if res[s] == res[k]*5:
+            if res[s] == res[k] * 5:
                 k += 1
         return res.pop()
