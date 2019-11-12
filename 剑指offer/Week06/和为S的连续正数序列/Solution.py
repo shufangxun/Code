2cbd7f3f57d009aee9eddaf886140611e0f001a6
shufangxun <1623062935@qq.com>
2019-11-12 22:53:09 +0800

    和为s的连续整数序列

--- 剑指offer/Week06/和为S的连续正数序列/Solution.py
+++ 剑指offer/Week06/和为S的连续正数序列/Solution.py
@@ -7,17 +7,17 @@ class Solution(object):
         res = []
         low, high = 1, 2
         while low < high:
+            # 计算当前和: 等差数列
             curSum = (low + high) * (high - low + 1) // 2
             if curSum == sum:
                 elem = []
                 for i in range(low, high+1):
                     elem.append(i)
                 res.append(elem)
-                low += 1
+                low += 1 # 细节
             elif curSum < sum:
                 high += 1
             else:
                 low += 1
-        
         return res
 
