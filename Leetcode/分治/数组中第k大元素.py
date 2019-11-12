d888d1cfd28bb784b92b91a82aa2a84ab3577c9f
shufangxun <1623062935@qq.com>
2019-11-12 10:53:48 +0800

    Update 数组中第k大元素.py

--- Leetcode/分治/数组中第k大元素.py
+++ Leetcode/分治/数组中第k大元素.py
@@ -3,13 +3,13 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         if k > len(nums):
             return
-        index = randint(0, len(nums)-1)
+        index = randint(0, len(nums) - 1)
         pivot = nums[index]
-        less_part = [i for i in nums[:index] + nums[index+1:] if i < pivot]
-        great_part = [i for i in nums[:index] + nums[index+1:] if i >= pivot]
-        if len(great_part) == k-1:
+        less_part = [i for i in nums[:index] + nums[index + 1:] if i < pivot]
+        great_part = [i for i in nums[:index] + nums[index + 1:] if i >= pivot]
+        if len(great_part) == k - 1:
             return pivot
-        elif len(great_part) > k-1:
-            return self.findKthLargest(great_part,k)
+        elif len(great_part) > k - 1:
+            return self.findKthLargest(great_part, k)
         else:
-            return self.findKthLargest(less_part,k-len(great_part)-1)
+            return self.findKthLargest(less_part, k - len(great_part) - 1)
