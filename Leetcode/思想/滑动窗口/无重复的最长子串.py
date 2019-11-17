31d4efd91bc45532b8315ca5bd7d38b203bc848c
shufangxun <1623062935@qq.com>
2019-11-17 21:47:33 +0800

    无重复的最长子串  最小覆盖子串 模板

--- Leetcode/思想/滑动窗口/无重复的最长子串.py
+++ Leetcode/思想/滑动窗口/无重复的最长子串.py
@@ -1,3 +1,4 @@
+# 法1
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -8,8 +9,36 @@ class Solution(object):
         ans = 0
         lookup = [-1] * 128
         for i in range(len(s)):
+            # 前一个出现在窗口内
             if lookup[ord(s[i])] > left:
                 left = lookup[ord(s[i])]
             ans = max(ans, i - left)
             lookup[ord(s[i])] = i
         return ans
+
+# 法2
+def lengthOfLongestSubstring(s):
+    lookup = [0] * 128
+    left, right = 0, 0
+    counter = 0
+    maxlen = 0
+    while right < len(s):
+        # 当右指针元素之前出现过，计数加１
+        if lookup[ord(s[right])] > 0:
+            counter += 1
+        # 不管出没出现，都需要统计出现次数，移动右指针
+        lookup[ord(s[right])] += 1
+        right += 1
+        
+        while counter > 0:
+            # 当有重复时
+            if lookup[ord(s[left])] > 1:
+                counter -= 1
+            # 这一步操作不懂
+            lookup[ord(s[left])] -= 1
+            left += 1
+        maxlen = max(maxlen, right - left)
+    return maxlen
+
+            
+
--- Leetcode/思想/滑动窗口/最小覆盖子串.py
+++ Leetcode/思想/滑动窗口/最小覆盖子串.py
@@ -1,3 +1,4 @@
+# 第一种
 class Solution(object):
     def minWindow(self, s: str, t: str) -> str:
         valid = [0] * 128
@@ -19,12 +20,43 @@ class Solution(object):
             right += 1
             # 当所有子串都包含
             while counter == 0:
+                # 移动左指针会使得窗口不符合条件时
+                if valid[ord(s[left])] == 0:
+                    counter += 1
+                if right - left + 1 <= minlen:
+                    minlen, res = right - left + 1, s[left:right+1]
                 # 恢复计数，因为之前做了减1操作
                 valid[ord(s[left])] += 1
-                # 移动左指针会使得窗口不符合条件时
-                if valid[ord(s[left])] > 0:
-                    counter += 1
-                    if right - left + 1 <= minlen:
-                        minlen, res = right - left + 1, s[left:right+1]
                 left += 1
         return res
+
+# 第二种
+def minWindow(self, s: str, t: str) -> str:
+    valid = [0] * 128
+    counter = len(t) # 子串是否符合条件
+    left, right = 0, 0
+    minlen = len(s) # 最小长度最坏情况下是 len(s)
+    res = "" # 子串
+    # 初始化 valid
+    for i in range(len(t)):
+        valid[ord(t[i])] += 1
+    # 找到可行解
+    while right < len(s):
+        # 当包含 t 字符时 counter - 1
+        if valid[ord(s[right])] > 0:
+            counter -= 1
+        # 包含的减1，不包含的变为负
+        valid[ord(s[right])] -= 1
+        # 当窗口包含所有 T 字符
+        while counter == 0:
+            # 恢复计数，因为之前做了减1操作
+            valid[ord(s[left])] += 1
+            # 当移动左指针会让窗口不符合条件时
+            if valid[ord(s[left])] > 0:
+                counter += 1
+                if right - left + 1 <= minlen:
+                    minlen, res = right - left + 1, s[left:right+1]
+            left += 1
+        # 不管是否找到，都移动右指针
+        right += 1
+    return res
