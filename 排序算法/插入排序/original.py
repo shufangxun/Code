b6a6d6ab37397e90395e20c4ccfc275fc8678571
shufangxun <1623062935@qq.com>
2019-11-20 23:37:19 +0800

    Update original.py

--- 排序算法/插入排序/original.py
+++ 排序算法/插入排序/original.py
@@ -2,14 +2,11 @@ def insertsort(arr):
     for i, item in enumerate(arr):
         index = i
         while index > 0 and arr[index - 1] > item:
-            arr[index] = arr[index - 1] 
+            arr[index] = arr[index - 1]
             index -= 1
-        
         arr[index] = item
-    
     return arr
-
 
 if __name__ == "__main__":
     a = [2,1,2,3]
-    print(insertsort(a)) 
+    print(insertsort(a))
