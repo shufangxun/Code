da85416db43e7b5939511207a35c6a022155a8fb
shufangxun <1623062935@qq.com>
2019-12-01 16:11:45 +0800

    Update test.py

--- Leetcode/test.py
+++ Leetcode/test.py
@@ -1,24 +1,22 @@
-def lengthOfLongestSubstring(s):
-        lookup = {}
-        i, ans = 0, 0
-        for j in range(len(s)):
-            if s[j] in lookup:
-                i = max(lookup[s[j]], i)
-            ans = max(ans, j - i + 1)
-            lookup[s[j]] = j + 1
-        return ans
+def coinChange1(coins, m):
+        f = [float('inf')]*(m+1)
+        f[0] = 0
+        for i in range(1, m+1):
+            for c in coins:
+                if i - c >= 0:
+                    f[i] = min(f[i], f[i-c]+1)
+        return f[m] if f[m] != float('inf') else -1  # 如果为inf说明状态不可达，返回-1即可。
 
-def test(s):
-    left, ans = -1, 0
-    lookup = {}
-    for i in range(len(s)):
-        if s[i] in lookup:
-        # 前一个出现在窗口内
-            if lookup[s[i]] > left:
-                left = lookup[s[i]]
-        ans = max(ans, i - left)
-        lookup[s[i]] = i
-    return ans
-s = list([1,2,4,2])
-a = test(s)
-print(a)
+def coinChange2(coins, m):
+    f = [float('inf')] * (m + 1)
+    f[0] = 0
+    for c in coins:  # 枚举硬币总数
+        for j in range(m, c-1, -1):  # 从大到小枚举金额，确保j-c >= 0.
+            f[j] = min(f[j], f[j - c] + 1)
+    return f[m] if f[m] != float('inf') else -1  # 如果为inf说明状态不可达，返回-1即可。
+
+coins = list([1,2,4,5,10])
+total1 = 20
+total2 = 11
+print(coinChange1(coins, total1))
+print(coinChange2(coins, total2))
