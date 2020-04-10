f2c7d7f28cc5c06d223238fb1a31ed66658b66d8
shufangxun <1623062935@qq.com>
2020-04-10 14:46:56 +0800

    括号生成回溯模版

--- Leetcode/算法思想/回溯/括号生成.py
+++ Leetcode/算法思想/回溯/括号生成.py
@@ -13,3 +13,25 @@ class Solution:
             self.f(sublist + '(', res, left-1, right)
         if right > 0:
             self.f(sublist + ')', res, left, right-1)
+
+# 回溯法
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        curlist = []
+        self.dfs(curlist, res, n, n)
+        return res
+    def dfs(self, curlist, res, left, right):
+        if left == 0 and right == 0:
+            res.append(''.join(curlist))
+            return 
+        if right < left or left < 0 or right < 0:
+            return
+        
+        curlist.append('(')
+        self.dfs(curlist, res, left-1, right)
+        curlist.pop()
+
+        curlist.append(')')
+        self.dfs(curlist, res, left, right-1)
+        curlist.pop()
