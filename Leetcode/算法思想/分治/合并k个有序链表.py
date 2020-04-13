924736bf268771f6a40ff38e00029d2e8b982a5a
shufangxun <1623062935@qq.com>
2020-04-13 18:00:25 +0800

    合并k个有序链表

---
+++ Leetcode/算法思想/分治/合并k个有序链表.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return 
+        n = len(lists)
+        return self.merge(lists, 0, n - 1)
+    
+    # 分链表
+    def merge(self, lists, left, right):
+        if left == right:
+            return lists[left]
+        mid = left + (right - left) // 2
+        l1 = self.merge(lists, left, mid)
+        l2 = self.merge(lists, mid+1, right)
+        return self.mergeTwoLists(l1, l2)
+
+    # 合并两个链表
+    def mergeTwoLists(self, l1, l2):
+        dummyhead = ListNode(0)
+        p = dummyhead
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = l1
+                l1 = l1.next
+            else:
+                p.next = l2
+                l2 = l2.next
+            p = p.next
+        p.next = l1 if l1 else l2
+        return dummyhead.next
