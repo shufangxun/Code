036e720c4ce499a2f07e554da8c30d45f6911c90
shufangxun <1623062935@qq.com>
2020-04-14 09:48:57 +0800

    归并排序 loop

--- 排序算法/归并排序/loop.py
+++ 排序算法/归并排序/loop.py
@@ -1,23 +1,8 @@
-def merge(left, right):
-    sorted = []
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            sorted.append(left[i])
-            i += 1
-        else:
-            sorted.append(right[j])
-            j += 1
-    sorted += left[i:]
-    sorted += right[j:]
-    
-    return sorted
-
 def bottomup_mergesort(arr):
     length = len(arr)
     size = 1
     while size < length:
-		size += size # 2
+		size *= 2
 		for pos in range(0, length, size):
 			sublist_start = pos
 			sublist_mid   = pos + (size / 2)
@@ -27,4 +12,19 @@ def bottomup_mergesort(arr):
 			list[sublist_start:sublist_end] = merge(left, right)
     return list
     
+def merge(left, right):
+    sorted = []
+    i, j = 0, 0
 
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted.append(left[i])
+            i += 1
+        else:
+            sorted.append(right[j])
+            j += 1
+
+    sorted += left[i:]
+    sorted += right[j:]
+    
+    return sorted
